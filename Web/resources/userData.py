9f1acd0c61f8b58eeb68822990c49f2130f6ca7c
RafaelFelice <rafaelfelice92@gmail.com>
2024-09-22 14:41:26 -0300

    ajustando python-package

---
+++ Web/resources/userData.py
@@ -0,0 +1,15 @@
+#User datas
+default_email = "fulano@qa.com"
+default_password = "teste"
+invalid_email = "invalid@qa.com"
+invalid_password = "invalid"
+blank_name = ""
+blank_email = ""
+blank_password = ""
+
+#Message return
+msg_invalid_email_or_password = "Email e/ou senha inválidos"
+msg_invalid_email_registered = "Este email já está sendo usado"
+msg_blank_name = "Nome é obrigatório"
+msg_blank_email = "Email é obrigatório"
+msg_blank_password = "Password é obrigatório"
